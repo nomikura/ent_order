5f8ac89b64b836c55876821ec34b32d0e4564393
nomikura <20913381+nomikura@users.noreply.github.com>
2023-06-15 13:49:09 +0900

    first commit

---
+++ ent/schema/user.go
@@ -0,0 +1,16 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+)
+
+type User struct {
+	ent.Schema
+}
+
+func (User) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("university"),
+	}
+}
