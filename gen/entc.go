a9e9e085a8dd7363f2f175465d7b36a22427f14d
nomikura <20913381+nomikura@users.noreply.github.com>
2023-06-16 10:11:58 +0900

    fix

--- gen/entc.go
+++ gen/entc.go
@@ -9,28 +9,12 @@ import (
 )
 
 func main() {
-	ex, err := entgql.NewExtension(
-	// entgql.WithWhereInputs(true),                  // GraphQLファイルにXxxWhereInput型を生成する
-	// entgql.WithConfigPath("../gqlgen.yml"), // 参照するGraphQLの設定ファイル
-	// entgql.WithSchemaGenerator(),                  // entからGraphQLの型定義を生成
-	// entgql.WithSchemaPath("../graph/ent.graphql"), // entから生成するGraphQLファイルのパス
-	)
+	ex, err := entgql.NewExtension()
 	if err != nil {
 		log.Fatalf("creating entgql extension: %v", err)
 	}
-	_ = ex
 
-	opts := []entc.Option{
-		entc.Extensions(ex),
-		// entc.FeatureNames(
-		// 	"sql/execquery",   // entのclientから生のSQLを実行できるようにする
-		// 	"schema/snapshot", // スキーマのスナップショットを作成して、コード生成が破綻しないようにする
-		// 	"sql/upsert",      // upsertを使えるようにする
-		// 	"privacy",
-		// ),
-	}
-
-	if err := entc.Generate("../ent/schema", &gen.Config{}, opts...); err != nil {
+	if err := entc.Generate("../ent/schema", &gen.Config{}, entc.Extensions(ex)); err != nil {
 		log.Fatalf("running ent codegen: %v", err)
 	}
 }
